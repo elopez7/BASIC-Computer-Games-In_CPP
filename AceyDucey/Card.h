3b96e8929bb1be6119a29e038fd96df9f16c136b
Esteban Lopez <e.lopez7@hotmail.com>
2026-03-31 12:43:04 -0400

    Improved the Card class

--- AceyDucey/Card.h
+++ AceyDucey/Card.h
@@ -1,17 +1,37 @@
 #ifndef CARD_H
 #define CARD_H
-#include <string>
+#include <compare>
+#include <format>
+#include <array>
+#include <string_view>
+#include <cassert>
 
 class Card {
 public:
-    Card() = default;
-    explicit Card(int value);
-    virtual ~Card() = default;
-    int GetValue() const;
-
-private:
-    std::string to_string() const;
-    int value_;
+    Card() = delete;
+    explicit Card(int value) : value_{ value } {
+        assert(value >= 2 && value <= 14);
+    }
+    ~Card() = default;
+    auto operator<=>(const Card& other) const = default;
+    const int value_;
 };
+
+template<>
+struct std::formatter<Card>{
+    constexpr auto parse(std::format_parse_context& ctx) {
+        return ctx.begin();
+    }
+
+    auto format(const Card& card, std::format_context& ctx) const {
+        static constexpr std::array<std::string_view, 4> card_letters{"Jack", "Queen", "King", "Ace"};
+        
+        if (card.value_ >= 11) {
+            return std::format_to(ctx.out(), "{}", card_letters[card.value_ - 11]);
+        }
+        return std::format_to(ctx.out(), "{}", card.value_);
+    }
+};
+
 
 #endif // !CARD_H
